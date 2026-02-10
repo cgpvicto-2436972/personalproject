0bc9171498187079e1cf9f323dbcdbebbb9db74b
Bryan Dubois <2436972@etudiant.cegepvicto.ca>
2026-02-10 10:58:26 -0500

    initialisation pokemon (NF)

--- pkprojectsln/pkproject/Program.cs
+++ pkprojectsln/pkproject/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace pkproject
+﻿using biblioclasse;
+namespace pkproject
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+
+        static void InitialisationPk()
+        {
+
+        }
+
+        static void ChoixPk()
+        {
+
         }
     }
 }
